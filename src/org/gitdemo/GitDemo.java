a1f8a1dd9c313614e75d308cc7f5792ebbc0ebcc
kavitasha123 <kavithatarini@gmail.com>
2021-12-07 10:51:26 +0530

    Username functionality completed

--- src/org/gitdemo/GitDemo.java
+++ src/org/gitdemo/GitDemo.java
@@ -1,5 +1,14 @@
 package org.gitdemo;
 
 public class GitDemo {
+	public static void userName() {
+		
+		System.out.println("Username Functionality");
+	}
+	
+	public static void main(String[]args) {
+		userName();
+		
+	}
 
 }
